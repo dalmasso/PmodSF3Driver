c5b61e904ad22e7f196ed16e43b7fb107435b6fa
root <loic.dalmasso@gmail.com>
2025-03-06 07:45:14 +0000

    update SPI Modes controller

--- simulations/Testbench_PmodSF3SPIModes.vhd
+++ simulations/Testbench_PmodSF3SPIModes.vhd
@@ -24,6 +24,7 @@
 -- Ports
 --		Input 	-	i_sys_clock: System Input Clock
 --		Input 	-	i_reset: Module Reset ('0': No Reset, '1': Reset)
+--		Input 	-	i_end_of_tx: End of SPI Transmission ('0': In progress, '1': End of Transmission)
 --		Input 	-	i_command: Command Byte
 --		Input 	-	i_new_data_to_mem: New Data to Write on FLASH Ready (Write Mode) ('0': NOT Ready, '1': Ready)
 --		Input 	-	i_data_to_mem: Data Bytes to Write on FLASH
@@ -48,6 +49,7 @@ COMPONENT PmodSF3SPIModes is
 PORT(
 	i_sys_clock: IN STD_LOGIC;
 	i_reset: IN STD_LOGIC;
+	i_end_of_tx: IN STD_LOGIC;
     i_command: IN UNSIGNED(7 downto 0);
 	i_new_data_to_mem: IN STD_LOGIC;
     i_data_to_mem: IN UNSIGNED(7 downto 0);
@@ -61,12 +63,14 @@ PORT(
 END COMPONENT;
 
 signal sys_clock: STD_LOGIC := '0';
+signal sys_clock_2: STD_LOGIC := '0';
 signal reset: STD_LOGIC := '0';
+signal end_of_tx: STD_LOGIC := '0';
 signal command: UNSIGNED(7 downto 0) := (others => '0');
 signal new_data_to_mem: STD_LOGIC := '0';
-signal data_to_mem: UNSIGNED(7 downto 0) := (others => '0');
+signal data_to_mem: UNSIGNED(7 downto 0) := x"AB";
 signal data_from_mem_ready: STD_LOGIC := '0';
-signal data_from_mem: UNSIGNED(7 downto 0) := (others => '0');
+signal data_from_mem: UNSIGNED(7 downto 0) := x"CD";
 signal spi_single_enable: STD_LOGIC := '0';
 signal spi_dual_enable: STD_LOGIC := '0';
 signal spi_quad_enable: STD_LOGIC := '0';
@@ -76,21 +80,35 @@ begin
 -- Clock 100 MHz
 sys_clock <= not(sys_clock) after 5 ns;
 
+-- Command Clock
+process
+begin
+    sys_clock_2 <= not(sys_clock_2) after 5 ns;
+    wait for 80 ns;
+end process;
+
 -- Reset
-reset <= '1', '0' after 145 ns;
+reset <= '1', '0' after 50 ns;
+
+-- End of Transmission
+end_of_tx <='1', '0' after 145 ns,
+            -- End after Write Enhanced Volatile
+            '1' after 15.685 us, '0' after 15.845 us,
+            -- End after Write Non Volatile
+            '1' after 28.485 us, '0' after 28.645 us;
 
 -- Command
-process(sys_clock)
+process(sys_clock_2)
 begin
-    if rising_edge(sys_clock) then
-        
-        -- Reset Command
-        if (reset = '1') then
-            command <= (others => '0');
-        
+    if rising_edge(sys_clock_2) then
+
         -- Increment Command
-        else
+        if (command < "11111111") then
             command <= command +1;
+        
+        -- Apply Non Volatile Dummy Cycle Register
+        else
+            command <= x"99";
         end if;
     end if;
 end process;
@@ -100,11 +118,11 @@ process(sys_clock)
 begin
     if rising_edge(sys_clock) then
         
-        -- Reset Data to Memory
+        -- No Data to Memory
         if (reset = '1') then
             new_data_to_mem <= '0';
         
-        -- Increment Data to Memory
+        -- New Data to Memory
         else
             new_data_to_mem <= '1';
         end if;
@@ -116,39 +134,26 @@ process(sys_clock)
 begin
     if rising_edge(sys_clock) then
         
-        -- Reset Data to Memory
-        if (reset = '1') then
-            data_to_mem <= (others => '0');
-        
         -- Increment Data to Memory
-        elsif(data_to_mem < "11111111") then
-            data_to_mem <= data_to_mem +1;
-            
-        end if;
+        data_to_mem <= data_to_mem +2;
+
     end if;
 end process;
 
 -- Data from Memory Ready
 data_from_mem_ready <=  '0',
                         '1' after 1.105 us, '0' after 1.175 us,
-                        '1' after 1.465 us, '0' after 1.535 us,
-                        '1' after 1.945 us, '0' after 2 us,
-                        '1' after 233 us, '0' after 280 us,
-                        '1' after 656.505 us, '0' after 656.525 us;
+                        '1' after 16.005 us, '0' after 16.165 us,
+                        '1' after 28.805 us, '0' after 28.965 us;
 
 -- Data from Memory for Test
 process(sys_clock)
 begin
     if rising_edge(sys_clock) then
-        
-        -- Reset Data to Memory
-        if (reset = '1') then
-            data_from_mem <= (others => '1');
-        
-        -- Increment Data to Memory
-        elsif (data_from_mem > "00000000") then
-            data_from_mem <= data_from_mem -1;
-        end if;
+
+        -- Decrement Data to Memory
+        data_from_mem <= data_from_mem -3;
+
     end if;
 end process;
 
@@ -156,6 +161,7 @@ uut: PmodSF3SPIModes
     PORT map(
         i_sys_clock => sys_clock,
         i_reset => reset,
+        i_end_of_tx => end_of_tx,
         i_command => command,
         i_new_data_to_mem => new_data_to_mem,
 		i_data_to_mem => data_to_mem,
--- sources/PmodSF3SPIModes.vhd
+++ sources/PmodSF3SPIModes.vhd
@@ -24,6 +24,7 @@
 -- Ports
 --		Input 	-	i_sys_clock: System Input Clock
 --		Input 	-	i_reset: Module Reset ('0': No Reset, '1': Reset)
+--		Input 	-	i_end_of_tx: End of SPI Transmission ('0': In progress, '1': End of Transmission)
 --		Input 	-	i_command: Command Byte
 --		Input 	-	i_new_data_to_mem: New Data to Write on FLASH Ready (Write Mode) ('0': NOT Ready, '1': Ready)
 --		Input 	-	i_data_to_mem: Data Bytes to Write on FLASH
@@ -43,6 +44,7 @@ ENTITY PmodSF3SPIModes is
 PORT(
 	i_sys_clock: IN STD_LOGIC;
 	i_reset: IN STD_LOGIC;
+	i_end_of_tx: IN STD_LOGIC;
     i_command: IN UNSIGNED(7 downto 0);
 	i_new_data_to_mem: IN STD_LOGIC;
     i_data_to_mem: IN UNSIGNED(7 downto 0);
@@ -81,19 +83,23 @@ constant SPI_SINGLE_MODE: UNSIGNED(1 downto 0) := "11";
 constant SPI_DUAL_MODE: UNSIGNED(1 downto 0) := "10";
 constant SPI_QUAD_MODE: UNSIGNED(0 downto 0) := "0";
 
+-- SPI Mode Write Bit Counter
+constant SPI_BIT_COUNTER_INIT: UNSIGNED(3 downto 0) := "0111";
+constant SPI_BIT_COUNTER_END: UNSIGNED(3 downto 0) := "1111";
+
 ------------------------------------------------------------------------
 -- Signal Declarations
 ------------------------------------------------------------------------
--- Non Volatile SPI Mode (Write mode)
-signal non_volatile_w_bit_counter: UNSIGNED(2 downto 0) := (others => '0');
-signal non_volatile_w_second_byte: STD_LOGIC := '0';
-
--- Non Volatile SPI Mode (Read mode)
-signal non_volatile_r_byte_counter: UNSIGNED(0 downto 0) := (others => '0');
-signal non_volatile_r_second_byte: STD_LOGIC := '0';
+-- Non Volatile SPI Mode Byte
+signal non_volatile_bit_counter: UNSIGNED(3 downto 0) := SPI_BIT_COUNTER_INIT;
+signal non_volatile_first_byte: STD_LOGIC := '0';
+signal non_volatile_second_byte: STD_LOGIC := '0';
 
 -- Non Volatile SPI Mode Register
 signal non_volatile_spi_mode_reg: UNSIGNED(1 downto 0) := (others => '0');
+
+-- Enhanced Volatile SPI Mode Byte
+signal enhanced_volatile_first_byte: STD_LOGIC := '0';
 
 -- Enhanced Volatile SPI Mode Register
 signal enhanced_volatile_spi_mode_reg: UNSIGNED(1 downto 0) := (others => '0');
@@ -107,90 +113,159 @@ signal spi_mode_out_reg: UNSIGNED(1 downto 0) := (others => '0');
 ------------------------------------------------------------------------
 begin
 
-	----------------------------------------------
-	-- Non-Volatile Write Configuration Handler --
-	----------------------------------------------
+	---------------------------------------------------
+	-- Non-Volatile Write Byte Configuration Handler --
+	---------------------------------------------------
 	process(i_sys_clock)
 	begin
-
 		if rising_edge(i_sys_clock) then
 
-			-- Reset Bit Counter
-			if (i_command /= WRITE_NON_VOLATILE_COMMAND) then
-				non_volatile_w_bit_counter <= (others => '1');
-			
-			-- Decrement Bit Counter
-			elsif (i_new_data_to_mem = '1') then
-				non_volatile_w_bit_counter <= non_volatile_w_bit_counter -1;
+			-- First Bit to Write
+			if (i_end_of_tx = '1') then
+				non_volatile_bit_counter <= SPI_BIT_COUNTER_INIT;
+
+			-- Next Bit to Write
+			elsif (i_command = WRITE_NON_VOLATILE_COMMAND) and (i_new_data_to_mem = '1') and (non_volatile_bit_counter /= SPI_BIT_COUNTER_END) then
+				non_volatile_bit_counter <= non_volatile_bit_counter -1;
 			end if;
 		end if;
 	end process;
-	non_volatile_w_second_byte <= '1' when (non_volatile_w_bit_counter = "000") else '0';
 
-	----------------------------------------------
-	-- Non-Volatile Read Configuration Handler --
-	----------------------------------------------
+	--------------------------------------------------
+	-- Non-Volatile Read Byte Configuration Handler --
+	--------------------------------------------------
 	process(i_sys_clock)
 	begin
-
 		if rising_edge(i_sys_clock) then
 
-			-- Reset Byte Counter
-			if (i_command /= READ_NON_VOLATILE_COMMAND) then
-				non_volatile_r_byte_counter <= (others => '1');
-			
-			-- Decrement Byte Counter
-			elsif (i_data_from_mem_ready = '1') then
-				non_volatile_r_byte_counter <= non_volatile_r_byte_counter -1;
+			-- First Byte to Read
+			if (i_end_of_tx = '1') then
+				non_volatile_first_byte <= '1';
+
+			-- Next Byte to Read
+			elsif (i_command = READ_NON_VOLATILE_COMMAND) and (i_data_from_mem_ready = '1') then
+				non_volatile_first_byte <= '0';
 			end if;
 		end if;
 	end process;
-	non_volatile_r_second_byte <= '1' when (non_volatile_r_byte_counter = "0") else '0';
+
+
+	--------------------------------------------------------
+	-- Non-Volatile Read/Write Byte Configuration Handler --
+	--------------------------------------------------------
+	process(i_sys_clock)
+	begin
+		if rising_edge(i_sys_clock) then
+
+			-- First Byte to Read/Write
+			if (i_end_of_tx = '1') then
+				non_volatile_second_byte <= '0';
+			
+			-- Next Byte to Write
+			elsif (i_command = WRITE_NON_VOLATILE_COMMAND) then
+				
+				-- Second Byte to Write only
+				if (non_volatile_bit_counter = "00") then
+					non_volatile_second_byte <= '1';
+				else
+					non_volatile_second_byte <= '0';
+				end if;
+
+			-- Next Byte to Read
+			elsif (i_command = READ_NON_VOLATILE_COMMAND) then
+
+				-- Second Byte to Read only
+				if (non_volatile_first_byte = '1') then
+					non_volatile_second_byte <= '1';
+				else
+					non_volatile_second_byte <= '0';
+				end if;
+
+			end if;
+		end if;
+	end process;
 
 	----------------------------------------
 	-- Non-Volatile Configuration Handler --
 	----------------------------------------
 	process(i_sys_clock)
 	begin
-
 		if rising_edge(i_sys_clock) then
 
-			-- Write Non-Volatile Configuration Register
-			if (i_command = WRITE_NON_VOLATILE_COMMAND) and (non_volatile_w_second_byte = '1') then
-				non_volatile_spi_mode_reg <= i_data_to_mem(NON_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_to_mem(NON_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+			-- Second Byte to Read/Write
+			if (non_volatile_second_byte = '1') then
 
-			-- Read Non-Volatile Configuration Register
-			elsif (i_command = READ_NON_VOLATILE_COMMAND) and (i_data_from_mem_ready = '1') and (non_volatile_r_second_byte = '1') then
-				non_volatile_spi_mode_reg <= i_data_from_mem(NON_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_from_mem(NON_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+				-- Write Non-Volatile Configuration Register
+				if (i_command = WRITE_NON_VOLATILE_COMMAND) then
+					non_volatile_spi_mode_reg <= i_data_to_mem(NON_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_to_mem(NON_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+
+				-- Read Non-Volatile Configuration Register
+				elsif (i_command = READ_NON_VOLATILE_COMMAND) then
+					non_volatile_spi_mode_reg <= i_data_from_mem(NON_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_from_mem(NON_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+				end if;
 			end if;
         end if;
     end process;
+
+	-------------------------------------------------------------
+	-- Enhanced Volatile Read/Write Byte Configuration Handler --
+	-------------------------------------------------------------
+	process(i_sys_clock)
+	begin
+		if rising_edge(i_sys_clock) then
+
+			-- First Byte to Read/Write
+			if (i_end_of_tx = '1') then
+				enhanced_volatile_first_byte <= '1';
+			
+			-- Next Byte to Write
+			elsif (i_command = WRITE_ENHANCED_VOLATILE_CONFIG_COMMAND) and (i_new_data_to_mem = '1') then
+				enhanced_volatile_first_byte <= '0';
+			
+			-- Next Byte to Read
+			elsif (i_command = READ_ENHANCED_VOLATILE_CONFIG_COMMAND) and (i_data_from_mem_ready = '1') then
+				enhanced_volatile_first_byte <= '0';
+			end if;
+		end if;
+	end process;
 
 	---------------------------------------------
 	-- Enhanced Volatile Configuration Handler --
 	---------------------------------------------
 	process(i_sys_clock)
 	begin
-
 		if rising_edge(i_sys_clock) then
 
-			-- Write Enhanced Volatile Configuration Register
-			if (i_command = WRITE_ENHANCED_VOLATILE_CONFIG_COMMAND) then
-				enhanced_volatile_spi_mode_reg <= i_data_to_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_to_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+			-- First Byte to Read/Write
+			if (enhanced_volatile_first_byte = '1') then
 
-			-- Read Enhanced Volatile Configuration Register
-			elsif (i_data_from_mem_ready = '1') and (i_command = READ_ENHANCED_VOLATILE_CONFIG_COMMAND) then
-				enhanced_volatile_spi_mode_reg <= i_data_from_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_from_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+				-- Write Enhanced Volatile Configuration Register
+				if (i_command = WRITE_ENHANCED_VOLATILE_CONFIG_COMMAND) then
+					enhanced_volatile_spi_mode_reg <= i_data_to_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_to_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+
+				-- Read Enhanced Volatile Configuration Register
+				elsif (i_data_from_mem_ready = '1') and (i_command = READ_ENHANCED_VOLATILE_CONFIG_COMMAND) then
+					enhanced_volatile_spi_mode_reg <= i_data_from_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_QUAD_BIT) & i_data_from_mem(ENHANCED_VOLATILE_CONFIG_REG_SPI_DUAL_BIT);
+				end if;
 			end if;
+		end if;
+	end process;
 
-            -- Apply New Enhanced Volatile Configuration Register to Output Register
+	-----------------------------------------------
+	-- Apply New Enhanced Volatile Configuration --
+	-----------------------------------------------
+	process(i_sys_clock)
+	begin
+		if rising_edge(i_sys_clock) then
+
+			-- Apply New Enhanced Volatile Configuration Register to Output Register
             if (i_command = WRITE_ENHANCED_VOLATILE_CONFIG_COMMAND) then
                 apply_enhanced_volatile_spi_mode <= '1';
             else
-                apply_enhanced_volatile_spi_mode <= '0';
+				apply_enhanced_volatile_spi_mode <= '0';
             end if;
 
-        end if;
+		end if;
     end process;
 
 	------------------------------
